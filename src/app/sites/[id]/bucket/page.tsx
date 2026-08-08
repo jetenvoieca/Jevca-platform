ae459cf20e301c554b289a91286188ef70a8f431
jetenvoieca <craig@isendyouthis.com>
2026-08-08 08:36:45 +0200

    Update page.tsx

--- src/app/sites/[id]/bucket/page.tsx
+++ src/app/sites/[id]/bucket/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 import { getDraftTimeline } from "@/lib/actions/videoEditor";
 import { getRenderStatus } from "@/lib/actions/render";
+import { getMediaTagPresets, getArtistArtworksForLinking } from "@/lib/actions/mediaCatalogue";
 import VideoEditorView from "@/components/VideoEditorView";
 
 export const dynamic = "force-dynamic";
@@ -14,12 +15,21 @@ export default async function BucketPage({
   const site = await db.site.findUnique({ where: { id }, select: { artistId: true } });
   const artistId = site!.artistId;
 
-  const [{ renderId, clips }, renderStatus] = await Promise.all([
+  const [{ renderId, clips }, renderStatus, tagPresets, artistArtworks] = await Promise.all([
     getDraftTimeline(artistId),
     getRenderStatus(artistId),
+    getMediaTagPresets(artistId),
+    getArtistArtworksForLinking(artistId),
   ]);
 
   return (
-    <VideoEditorView siteId={id} renderId={renderId} initialClips={clips} renderStatus={renderStatus} />
+    <VideoEditorView
+      siteId={id}
+      renderId={renderId}
+      initialClips={clips}
+      renderStatus={renderStatus}
+      tagPresets={tagPresets}
+      artistArtworks={artistArtworks}
+    />
   );
 }
